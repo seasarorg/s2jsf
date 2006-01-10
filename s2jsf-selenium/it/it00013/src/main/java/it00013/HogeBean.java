1b3f0247d8223e2c92d88f9bc54d01814d201b21
manhole <manholex@gmail.com>
2006-01-10 10:43:47 +0000

    [Seasar-user:3078]
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@152 54d65437-b504-0410-8c2e-c3e44261fb90

---
+++ s2jsf-selenium/it/it00013/src/main/java/it00013/HogeBean.java
@@ -0,0 +1,16 @@
+package it00013;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HogeBean {
+
+    public List getList() {
+        List list = new ArrayList();
+        list.add("あ");
+        list.add("い");
+        list.add("う");
+        return list;
+    }
+
+}
---
+++ s2jsf-selenium/it/it00013/src/test/java/it00013/It00013Test.java
@@ -0,0 +1,44 @@
+package it00013;
+
+import junit.framework.Test;
+
+import org.seasar.jsf.selenium.SeleneseTestCase;
+import org.seasar.jsf.selenium.WebApplicationTestSetup;
+
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+/*
+ * [#7773][Seasar-user:3078]
+ * 
+ * spanタグのm:rendered=false時に、UIElement#afterContentsが出力されない問題。
+ */
+public class It00013Test extends SeleneseTestCase {
+
+    public static Test suite() {
+        WebApplicationTestSetup seleniumTestSetup = new WebApplicationTestSetup(
+            It00013Test.class);
+        return seleniumTestSetup;
+    }
+
+    public void testHoge() throws Exception {
+        // ## Arrange ##
+        Selenium selenium = getSelenium();
+
+        // ## Act ##
+        // ## Assert ##
+        selenium.open("/hoge.html");
+        selenium.verifyTextPresent("A");
+        try {
+            selenium.verifyTextPresent("B");
+            fail();
+        } catch (SeleniumException expected) {
+        }
+        selenium.verifyTextPresent("C");
+        selenium.verifyTextPresent("D");
+        selenium.verifyTextPresent("E");
+
+        selenium.testComplete();
+    }
+
+}
