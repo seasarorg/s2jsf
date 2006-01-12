428ce1a10a3b0c4c1fc30fe0081ff740911e95c8
manhole <manholex@gmail.com>
2006-01-12 10:22:29 +0000

    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@160 54d65437-b504-0410-8c2e-c3e44261fb90

--- s2jsf-example/WEB-INF/src/examples/jsf/action/impl/ForEach2ListActionImpl.java
+++ s2jsf-example/WEB-INF/src/examples/jsf/action/impl/ForEach2ListActionImpl.java
@@ -3,31 +3,29 @@ package examples.jsf.action.impl;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import examples.jsf.action.ForEach2ListAction;
 import examples.jsf.dto.ForEach2Dto;
 
 public class ForEach2ListActionImpl implements ForEach2ListAction {
 
-	private List forEach2DtoList;
-	
-	public void setForEach2DtoList(List forEach2DtoList) {
-		this.forEach2DtoList = forEach2DtoList;
-	}
-	
-	public String update() {
-		for (Iterator i = forEach2DtoList.iterator(); i.hasNext(); ) {
-			ForEach2Dto dto = (ForEach2Dto) i.next();
-			if (dto.isDelete()) {
-				i.remove();
-			}
-		}
-		return null;
-	}
+    private List forEach2DtoList;
 
-	public String addRow() {
-		forEach2DtoList.add(new ForEach2Dto());
-		return null;
-	}
+    public void setForEach2DtoList(List forEach2DtoList) {
+        this.forEach2DtoList = forEach2DtoList;
+    }
+
+    public String update() {
+        for (Iterator i = forEach2DtoList.iterator(); i.hasNext();) {
+            ForEach2Dto dto = (ForEach2Dto) i.next();
+            if (dto.isDelete()) {
+                i.remove();
+            }
+        }
+        return null;
+    }
+
+    public String addRow() {
+        forEach2DtoList.add(new ForEach2Dto());
+        return null;
+    }
 }
