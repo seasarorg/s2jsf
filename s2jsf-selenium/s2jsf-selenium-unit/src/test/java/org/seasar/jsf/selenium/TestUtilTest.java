d7148d0f4c675cf6bf06517a1d1df8fe1fe14e6c
manhole <manholex@gmail.com>
2006-02-17 13:23:55 +0000

    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@199 54d65437-b504-0410-8c2e-c3e44261fb90

--- s2jsf-selenium/s2jsf-selenium-unit/src/test/java/org/seasar/jsf/selenium/TestUtilTest.java
+++ s2jsf-selenium/s2jsf-selenium-unit/src/test/java/org/seasar/jsf/selenium/TestUtilTest.java
@@ -9,28 +9,29 @@ public class TestUtilTest extends TestCase {
     public void testGetProjectRootPath() throws Exception {
         String path = TestUtil.getProjectRootPath();
         System.out.println(path);
-        assertEquals(true, path.endsWith(File.separatorChar + "s2jsf-selenium"));
+        assertEquals(true, path.endsWith(File.separatorChar + "s2jsf-selenium")
+            || path.endsWith(File.separatorChar + "s2jsf-selenium-unit"));
     }
 
     public void testGetProjectPath() throws Exception {
         String path = TestUtil.getProjectPath("s2jsf-selenium-unit");
         System.out.println(path);
         assertEquals(true, path.endsWith(File.separatorChar
-                + "s2jsf-selenium-unit"));
+            + "s2jsf-selenium-unit"));
     }
 
     public void testGetCurrentProjectPath() throws Exception {
         String path = TestUtil.getCurrentProjectPath();
         System.out.println(path);
         assertEquals(true, path.endsWith(File.separatorChar
-                + "s2jsf-selenium-unit"));
+            + "s2jsf-selenium-unit"));
     }
 
     public void testGetProjectPathByClass() throws Exception {
         String path = TestUtil.getProjectPathByClass(TestUtilTest.class);
         System.out.println(path);
         assertEquals(true, path.endsWith(File.separatorChar
-                + "s2jsf-selenium-unit"));
+            + "s2jsf-selenium-unit"));
     }
 
 }
