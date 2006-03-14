d996e4ba0b1461fcdcf7f48362cf3344361ffacd
manhole <manholex@gmail.com>
2006-03-14 14:25:25 +0000

    add: - InputSecret - The page transition with exception
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@241 54d65437-b504-0410-8c2e-c3e44261fb90

---
+++ s2jsf-example-test/src/test/java/org/seasar/jsf/example/it/InputSecretTest.java
@@ -0,0 +1,52 @@
+package org.seasar.jsf.example.it;
+
+import org.jaxen.JaxenException;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.xpath.HtmlUnitXPath;
+
+/**
+ * @author manhole
+ */
+public class InputSecretTest extends AbstractTestCase {
+
+    public void test1() throws Exception {
+        HtmlPage page1 = getPageFromMenu("InputSecret");
+        String body1 = getBody(page1).trim();
+        System.out.println(body1);
+
+        assertEquals("InputSecret", page1.getTitleText());
+
+        // 1
+
+        HtmlPasswordInput a1 = getPassword(page1);
+        a1.setValueAttribute("abcde");
+
+        HtmlSubmitInput submit1 = getSubmit(page1);
+
+        // 2
+
+        HtmlPage page2 = (HtmlPage) submit1.click();
+        String body2 = getBody(page2).trim();
+        System.out.println(body2);
+
+        HtmlPasswordInput a2 = getPassword(page2);
+        assertEquals("", a2.getValueAttribute());
+    }
+
+    private HtmlPasswordInput getPassword(HtmlPage page) throws JaxenException {
+        HtmlForm form = getForm(page);
+        return (HtmlPasswordInput) new HtmlUnitXPath(
+                ".//input[@type='password']").selectSingleNode(form);
+    }
+
+    private HtmlSubmitInput getSubmit(HtmlPage page) throws JaxenException {
+        HtmlForm form = getForm(page);
+        return (HtmlSubmitInput) new HtmlUnitXPath("//input[@type='submit']")
+                .selectSingleNode(form);
+    }
+
+}
---
+++ s2jsf-example-test/src/test/java/org/seasar/jsf/example/it/PageTransitionWithExceptionTest.java
@@ -0,0 +1,70 @@
+package org.seasar.jsf.example.it;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import junitx.framework.StringAssert;
+
+import org.jaxen.JaxenException;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.xpath.HtmlUnitXPath;
+
+/**
+ * @author manhole
+ */
+public class PageTransitionWithExceptionTest extends AbstractTestCase {
+
+    public void test1() throws Exception {
+        HtmlPage page1 = getPage();
+
+        // 1
+
+        HtmlSubmitInput a1 = getSubmitA(page1);
+
+        // 2
+
+        HtmlPage page2 = (HtmlPage) a1.click();
+        String body2 = getBody(page2).trim();
+        System.out.println(body2);
+        StringAssert.assertContains("errorPage1", body2);
+    }
+
+    private HtmlPage getPage() throws MalformedURLException, IOException {
+        HtmlPage page = getPageFromMenu("The page transition with exception");
+        String body1 = getBody(page).trim();
+        System.out.println(body1);
+        assertEquals("The page transition with exception", page.getTitleText());
+        return page;
+    }
+
+    public void test2() throws Exception {
+        HtmlPage page1 = getPage();
+
+        // 1
+
+        HtmlSubmitInput a1 = getSubmitB(page1);
+
+        // 2
+
+        HtmlPage page2 = (HtmlPage) a1.click();
+        String body2 = getBody(page2).trim();
+        System.out.println(body2);
+        StringAssert.assertContains("errorPage2", body2);
+    }
+
+    private HtmlSubmitInput getSubmitA(HtmlPage page) throws JaxenException {
+        HtmlForm form = getForm(page);
+        return (HtmlSubmitInput) new HtmlUnitXPath(
+                ".//input[@type='submit'][1]").selectSingleNode(form);
+    }
+
+    private HtmlSubmitInput getSubmitB(HtmlPage page) throws JaxenException {
+        HtmlForm form = getForm(page);
+        return (HtmlSubmitInput) new HtmlUnitXPath(
+                ".//input[@type='submit'][2]").selectSingleNode(form);
+    }
+
+}
