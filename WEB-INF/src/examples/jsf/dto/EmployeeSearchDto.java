d91c87695da53f846f127c6f9081c9b1f6bb05c2
manhole <manholex@gmail.com>
2006-03-16 09:01:02 +0000

    AutoRegisterized
    git-svn-id: https://www.seasar.org/svn/s2jsf/branches/s2jsf-example-1.0.x@259 54d65437-b504-0410-8c2e-c3e44261fb90

--- WEB-INF/src/examples/jsf/dto/EmployeeSearchDto.java
+++ WEB-INF/src/examples/jsf/dto/EmployeeSearchDto.java
@@ -5,110 +5,114 @@ import java.math.BigDecimal;
 
 public class EmployeeSearchDto implements Serializable {
 
-	private Integer empno;
+    public static final String COMPONENT = "instance = session";
 
-	private String ename;
+    private static final long serialVersionUID = 1L;
 
-	private String job;
+    private Integer empno;
 
-	private Short mgr;
+    private String ename;
 
-	private java.util.Date fromHiredate;
-	
-	private java.util.Date toHiredate;
+    private String job;
 
-	private BigDecimal fromSal;
-	
-	private BigDecimal toSal;
+    private Short mgr;
 
-	private Integer deptno;
+    private java.util.Date fromHiredate;
 
-	public EmployeeSearchDto() {
-	}
+    private java.util.Date toHiredate;
 
-	public Integer getEmpno() {
-		return this.empno;
-	}
+    private BigDecimal fromSal;
 
-	public void setEmpno(Integer empno) {
-		this.empno = empno;
-	}
+    private BigDecimal toSal;
 
-	public java.lang.String getEname() {
-		return this.ename;
-	}
+    private Integer deptno;
 
-	public void setEname(java.lang.String ename) {
-		this.ename = ename;
-	}
+    public EmployeeSearchDto() {
+    }
 
-	public java.lang.String getJob() {
-		return this.job;
-	}
+    public Integer getEmpno() {
+        return this.empno;
+    }
 
-	public void setJob(java.lang.String job) {
-		this.job = job;
-	}
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
 
-	public Short getMgr() {
-		return this.mgr;
-	}
+    public java.lang.String getEname() {
+        return this.ename;
+    }
 
-	public void setMgr(Short mgr) {
-		this.mgr = mgr;
-	}
+    public void setEname(java.lang.String ename) {
+        this.ename = ename;
+    }
 
-	public java.util.Date getFromHiredate() {
-		return this.fromHiredate;
-	}
+    public java.lang.String getJob() {
+        return this.job;
+    }
 
-	public void setFromHiredate(java.util.Date fromHiredate) {
-		this.fromHiredate = fromHiredate;
-	}
-	
-	public java.util.Date getToHiredate() {
-		return this.toHiredate;
-	}
+    public void setJob(java.lang.String job) {
+        this.job = job;
+    }
 
-	public void setToHiredate(java.util.Date toHiredate) {
-		this.toHiredate = toHiredate;
-	}
+    public Short getMgr() {
+        return this.mgr;
+    }
 
-	public BigDecimal getFromSal() {
-		return this.fromSal;
-	}
+    public void setMgr(Short mgr) {
+        this.mgr = mgr;
+    }
 
-	public void setFromSal(BigDecimal fromSal) {
-		this.fromSal = fromSal;
-	}
-	
-	public BigDecimal getToSal() {
-		return this.toSal;
-	}
+    public java.util.Date getFromHiredate() {
+        return this.fromHiredate;
+    }
 
-	public void setToSal(BigDecimal toSal) {
-		this.toSal = toSal;
-	}
+    public void setFromHiredate(java.util.Date fromHiredate) {
+        this.fromHiredate = fromHiredate;
+    }
 
-	public Integer getDeptno() {
-		return this.deptno;
-	}
+    public java.util.Date getToHiredate() {
+        return this.toHiredate;
+    }
 
-	public void setDeptno(Integer deptno) {
-		this.deptno = deptno;
-	}
+    public void setToHiredate(java.util.Date toHiredate) {
+        this.toHiredate = toHiredate;
+    }
 
-	public String toString() {
-		StringBuffer buf = new StringBuffer("[");
-		buf.append(empno).append(", ");
-		buf.append(ename).append(", ");
-		buf.append(job).append(", ");
-		buf.append(mgr).append(", ");
-		buf.append(fromHiredate).append(", ");
-		buf.append(toHiredate).append(", ");
-		buf.append(fromSal).append(", ");
-		buf.append(toSal).append(", ");
-		buf.append(deptno).append("]");
-		return buf.toString();
-	}
+    public BigDecimal getFromSal() {
+        return this.fromSal;
+    }
+
+    public void setFromSal(BigDecimal fromSal) {
+        this.fromSal = fromSal;
+    }
+
+    public BigDecimal getToSal() {
+        return this.toSal;
+    }
+
+    public void setToSal(BigDecimal toSal) {
+        this.toSal = toSal;
+    }
+
+    public Integer getDeptno() {
+        return this.deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public String toString() {
+        StringBuffer buf = new StringBuffer("[");
+        buf.append(empno).append(", ");
+        buf.append(ename).append(", ");
+        buf.append(job).append(", ");
+        buf.append(mgr).append(", ");
+        buf.append(fromHiredate).append(", ");
+        buf.append(toHiredate).append(", ");
+        buf.append(fromSal).append(", ");
+        buf.append(toSal).append(", ");
+        buf.append(deptno).append("]");
+        return buf.toString();
+    }
 }
--- WEB-INF/src/examples/jsf/dto/ProcessModeDto.java
+++ WEB-INF/src/examples/jsf/dto/ProcessModeDto.java
@@ -2,13 +2,16 @@ package examples.jsf.dto;
 
 public class ProcessModeDto {
 
-	private int processMode;
+    public static final String COMPONENT = "instance = session";
 
-	public int getProcessMode() {
-		return processMode;
-	}
-	
-	public void setProcessMode(int processMode) {
-		this.processMode = processMode;
-	}
+    private int processMode;
+
+    public int getProcessMode() {
+        return processMode;
+    }
+
+    public void setProcessMode(int processMode) {
+        this.processMode = processMode;
+    }
+
 }
