c8d9835e2e2628771de303d0f4c1b3df5cd40746
manhole <manholex@gmail.com>
2006-03-16 03:33:55 +0000

    find *.tld under WEB-INF
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk/s2jsf@250 54d65437-b504-0410-8c2e-c3e44261fb90

---
+++ src/test/java/org/seasar/jsf/runtime/FileSystemTaglibManagerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.jsf.runtime;
+
+import junit.framework.TestCase;
+
+import org.seasar.framework.mock.servlet.MockServletContextImpl;
+
+/**
+ * @author manhole
+ */
+public class FileSystemTaglibManagerTest extends TestCase {
+
+    public void test1() throws Exception {
+        // ## Arrange ##
+        FileSystemTaglibManager taglibManager = new FileSystemTaglibManager();
+        MockServletContextImpl servletContext = new MockServletContextImpl(null);
+        taglibManager.setServletContext(servletContext);
+
+        // ## Act ##
+        taglibManager
+                .init("org/seasar/jsf/runtime/FileSystemTaglibManagerTest-resource");
+
+        // ## Assert ##
+        assertEquals(true, taglibManager.hasTaglibConfig("a_uri"));
+        assertEquals(true, taglibManager.hasTaglibConfig("b_uri"));
+        assertEquals(true, taglibManager.hasTaglibConfig("c_uri"));
+        assertEquals(true, taglibManager.hasTaglibConfig("d_uri"));
+        assertEquals(false, taglibManager.hasTaglibConfig("e_uri"));
+    }
+
+}
