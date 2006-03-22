30023ca2d421954d80e0e329dd5cf5767b2fbfcf
manhole <manholex@gmail.com>
2006-03-22 23:18:29 +0000

    copy from branch
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@294 54d65437-b504-0410-8c2e-c3e44261fb90

---
+++ s2jsf-example/WEB-INF/src/test/examples/jsf/dao/EmployeeDtoDaoTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package test.examples.jsf.dao;
+
+import org.seasar.dao.unit.S2DaoTestCase;
+
+import examples.jsf.dao.EmployeeDtoDao;
+import examples.jsf.dto.EmployeeSearchDto;
+
+/**
+ * @author higa
+ *
+ */
+public class EmployeeDtoDaoTest extends S2DaoTestCase {
+
+	private EmployeeDtoDao employeeDtoDao_;
+
+	public EmployeeDtoDaoTest(String arg0) {
+		super(arg0);
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(EmployeeDtoDaoTest.class);
+	}
+	
+	public void setUp() {
+		include("examples/jsf/dicon/alldao.dicon");
+	}
+
+	public void testGetSearchCount() throws Exception {
+		EmployeeSearchDto searchDto = new EmployeeSearchDto();
+		searchDto.setEmpno(new Integer(7788));
+		assertEquals("1", 1, employeeDtoDao_.getSearchCount(searchDto));
+	}
+	
+	public void testSearchEmployeeDtoList() throws Exception {
+		EmployeeSearchDto searchDto = new EmployeeSearchDto();
+		searchDto.setEmpno(new Integer(7788));
+		assertNotNull("1", employeeDtoDao_.searchEmployeeDtoList(searchDto));
+	}
+	
+	public void testGetEmployeeDto() throws Exception {
+		assertNotNull("1", employeeDtoDao_.getEmployeeDto(new Integer(7788)));
+	}
+}
