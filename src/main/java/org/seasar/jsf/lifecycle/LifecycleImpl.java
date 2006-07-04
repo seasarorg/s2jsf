ef404e65f66c5c5bd4b9f61e0f665166b872932d
shot6 <shinpei.ohtani@gmail.com>
2006-07-04 06:27:29 +0000

    support postback.
    git-svn-id: https://www.seasar.org/svn/s2jsf/branches/s2jsf-1.0.x@391 54d65437-b504-0410-8c2e-c3e44261fb90

--- src/main/java/org/seasar/jsf/lifecycle/LifecycleImpl.java
+++ src/main/java/org/seasar/jsf/lifecycle/LifecycleImpl.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -53,6 +53,8 @@ public class LifecycleImpl extends Lifecycle {
 
     private static final long REDIRECT_WAIT_TIME = 2000;
 
+    private static final String POSTBACK_ATTR = "postback";
+
     private PhaseListener[] phaseListeners = new PhaseListener[0];
 
     public LifecycleImpl() {
@@ -61,6 +63,8 @@ public class LifecycleImpl extends Lifecycle {
     public void execute(FacesContext context) throws FacesException {
         try {
             boolean postback = restoreView(context);
+            context.getExternalContext().getRequestMap().put(POSTBACK_ATTR,
+                    Boolean.valueOf(postback));
             if (isFinished(context)) {
                 return;
             }
