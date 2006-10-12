1be4a8396ee3b6a679318efa93e23cf950d055d5
shot6 <shinpei.ohtani@gmail.com>
2006-10-12 12:17:33 +0000

    Seasar-user:4627
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@580 54d65437-b504-0410-8c2e-c3e44261fb90

--- s2jsf/src/main/java/org/seasar/jsf/component/html/S2HtmlInputText.java
+++ s2jsf/src/main/java/org/seasar/jsf/component/html/S2HtmlInputText.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -46,12 +46,20 @@ public class S2HtmlInputText extends HtmlInputText {
         this.label = label;
     }
 
+    public Object getValue() {
+        if (isLocalValueSet()) {
+            return getLocalValue();
+        }
+        return super.getValue();
+    }
+
     public void setValue(Object value) {
         Object newValue = value;
         if ("".equals(value)) {
             newValue = null;
         }
         super.setValue(newValue);
+        setLocalValueSet(true);
     }
 
     public void validate(FacesContext context) {
