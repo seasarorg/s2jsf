1f572fc92d5d2ba00c6a49d480a888cd73640931
yone098 <yone098@gmail.com>
2007-03-01 13:18:05 +0000

    JSF-50 nullLabelにValueBindng適用
    git-svn-id: https://www.seasar.org/svn/s2jsf/branches/s2jsf-1.0.x@747 54d65437-b504-0410-8c2e-c3e44261fb90

--- src/main/java/org/seasar/jsf/taglib/html/HtmlSelectTagBase.java
+++ src/main/java/org/seasar/jsf/taglib/html/HtmlSelectTagBase.java
@@ -24,93 +24,99 @@ import org.seasar.jsf.util.BindingUtil;
 
 /**
  * @author higa
- *
+ * 
  */
 public abstract class HtmlSelectTagBase extends HtmlInputTagBase {
-	
-	private String border;
-	
-	private String disabledClass;
 
-	private String enabledClass;
-	
-	private String items;
-	
-	private String itemLabel;
-	
-	private String itemValue;
-	
-	private String layout;
-	
-	private String nullLabel;
+    private String border;
 
-	public void setBorder(String border) {
-		this.border = border;
-	}
-	
-	public void setDisabledClass(String disabledClass) {
-		this.disabledClass = disabledClass;
-	}
+    private String disabledClass;
 
-	public void setEnabledClass(String enabledClass) {
-		this.enabledClass = enabledClass;
-	}
-	
-	public void setItems(String items) {
-		this.items = items;
-	}
-	
-	public void setItemLabel(String itemLabel) {
-		this.itemLabel = itemLabel;
-	}
-	
-	public void setItemValue(String itemValue) {
-		this.itemValue = itemValue;
-	}
-	
-	public void setLayout(String layout) {
-		this.layout = layout;
-	}
-	
-	public void setNullLabel(String nullLabel) {
-		this.nullLabel = nullLabel;
-	}
-	
-	public void release() {
-		super.release();
-		border = null;
-		disabledClass = null;
-		enabledClass = null;
-		items = null;
-		itemLabel = null;
-		itemValue = null;
-		layout = null;
-		nullLabel = null;
-	}
-	
-	protected void setProperties(UIComponent component) {
-		super.setProperties(component);
-		setComponentProperty(component, JsfConstants.BORDER_ATTR, border);
-		setComponentProperty(component, JsfConstants.DISABLED_CLASS_ATTR,
-				disabledClass);
-		setComponentProperty(component, JsfConstants.ENABLED_CLASS_ATTR, enabledClass);
-		setComponentProperty(component, JsfConstants.LAYOUT_ATTR, layout);
-		if (items != null) {
-			if (!isValueReference(items)) {
-				throw new NoValueReferenceRuntimeException(items);
-			}
-			S2UISelectItems child = new S2UISelectItems();
-			BindingUtil.setValueBinding(child, JsfConstants.VALUE_ATTR, items);
-			if (itemValue != null) {
-				child.setItemValue(itemValue);
-			}
-			if (itemLabel != null) {
-				child.setItemLabel(itemLabel);
-			}
-			if (nullLabel != null) {
-				child.setNullLabel(nullLabel);
-			}
-			component.getChildren().add(child);
-		}
-	}
+    private String enabledClass;
+
+    private String items;
+
+    private String itemLabel;
+
+    private String itemValue;
+
+    private String layout;
+
+    private String nullLabel;
+
+    public void setBorder(String border) {
+        this.border = border;
+    }
+
+    public void setDisabledClass(String disabledClass) {
+        this.disabledClass = disabledClass;
+    }
+
+    public void setEnabledClass(String enabledClass) {
+        this.enabledClass = enabledClass;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public void setItemLabel(String itemLabel) {
+        this.itemLabel = itemLabel;
+    }
+
+    public void setItemValue(String itemValue) {
+        this.itemValue = itemValue;
+    }
+
+    public void setLayout(String layout) {
+        this.layout = layout;
+    }
+
+    public void setNullLabel(String nullLabel) {
+        this.nullLabel = nullLabel;
+    }
+
+    public void release() {
+        super.release();
+        border = null;
+        disabledClass = null;
+        enabledClass = null;
+        items = null;
+        itemLabel = null;
+        itemValue = null;
+        layout = null;
+        nullLabel = null;
+    }
+
+    protected void setProperties(UIComponent component) {
+        super.setProperties(component);
+        setComponentProperty(component, JsfConstants.BORDER_ATTR, border);
+        setComponentProperty(component, JsfConstants.DISABLED_CLASS_ATTR,
+                disabledClass);
+        setComponentProperty(component, JsfConstants.ENABLED_CLASS_ATTR,
+                enabledClass);
+        setComponentProperty(component, JsfConstants.LAYOUT_ATTR, layout);
+        if (items != null) {
+            if (!isValueReference(items)) {
+                throw new NoValueReferenceRuntimeException(items);
+            }
+            S2UISelectItems child = new S2UISelectItems();
+            BindingUtil.setValueBinding(child, JsfConstants.VALUE_ATTR, items);
+            if (itemValue != null) {
+                child.setItemValue(itemValue);
+            }
+            if (itemLabel != null) {
+                child.setItemLabel(itemLabel);
+            }
+            if (nullLabel != null) {
+                if (isValueReference(nullLabel)) {
+                    child.setNullLabel(BindingUtil.resolveBinding(nullLabel)
+                            .toString());
+                } else {
+                    child.setNullLabel(nullLabel);
+                }
+            }
+            component.getChildren().add(child);
+        }
+    }
 }
