892623203548b72a3c1c5b3475581b09bff8c998
yone098 <yone098@gmail.com>
2007-03-29 18:05:29 +0000

    JIRA-60 idのレンダリングでgetClinetId()を利用する
    git-svn-id: https://www.seasar.org/svn/s2jsf/trunk@817 54d65437-b504-0410-8c2e-c3e44261fb90

--- s2jsf/src/main/java/org/seasar/jsf/render/html/HtmlElementRenderer.java
+++ s2jsf/src/main/java/org/seasar/jsf/render/html/HtmlElementRenderer.java
@@ -24,88 +24,86 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.render.Renderer;
 
-import org.seasar.jsf.JsfConstants;
 import org.seasar.jsf.component.UIElement;
 import org.seasar.jsf.util.BindingUtil;
 import org.seasar.jsf.util.RenderUtil;
 
 /**
  * @author higa
- *
+ * @author yone
  */
 public class HtmlElementRenderer extends Renderer {
 
-	public void encodeBegin(FacesContext facesContext, UIComponent component)
-			throws IOException {
+    public void encodeBegin(FacesContext facesContext, UIComponent component)
+            throws IOException {
 
-		if (!component.isRendered()) {
-			return;
-		}
-		ResponseWriter writer = facesContext.getResponseWriter();
-		if (!(component instanceof UIElement)) {
-			throw new IllegalArgumentException("not UIElementNode");
-		}
-		UIElement elem = (UIElement) component;
-		String tagName = elem.getTagName();
-		writer.startElement(tagName, component);
-		renderAttributes(writer, elem);
-	}
+        if (!component.isRendered()) {
+            return;
+        }
+        ResponseWriter writer = facesContext.getResponseWriter();
+        if (!(component instanceof UIElement)) {
+            throw new IllegalArgumentException("not UIElementNode");
+        }
+        UIElement elem = (UIElement) component;
+        String tagName = elem.getTagName();
+        writer.startElement(tagName, component);
+        if (elem.isIdSet()) {
+            RenderUtil.renderIdIfNecessary(writer, component, facesContext);
+        }
+        renderAttributes(writer, elem);
+    }
 
-	public void encodeEnd(FacesContext facesContext, UIComponent component)
-			throws IOException {
+    public void encodeEnd(FacesContext facesContext, UIComponent component)
+            throws IOException {
 
-	    UIElement elem = (UIElement) component;
-	    ResponseWriter writer = facesContext.getResponseWriter();
-		if (component.isRendered()) {
-		    writer.endElement(elem.getTagName());
-		}
+        UIElement elem = (UIElement) component;
+        ResponseWriter writer = facesContext.getResponseWriter();
+        if (component.isRendered()) {
+            writer.endElement(elem.getTagName());
+        }
         String afterContents = elem.getAfterContents();
         if (afterContents != null) {
             writer.write(afterContents);
         }
-	}
+    }
 
-	/**
-	 * @see javax.faces.render.Renderer#getRendersChildren()
-	 */
-	public boolean getRendersChildren() {
-		return true;
-	}
+    /**
+     * @see javax.faces.render.Renderer#getRendersChildren()
+     */
+    public boolean getRendersChildren() {
+        return true;
+    }
 
-	/**
-	 * @see javax.faces.render.Renderer#encodeChildren(javax.faces.context.FacesContext,
-	 *      javax.faces.component.UIComponent)
-	 */
-	public void encodeChildren(FacesContext context, UIComponent component)
-			throws IOException {
+    /**
+     * @see javax.faces.render.Renderer#encodeChildren(javax.faces.context.FacesContext,
+     *      javax.faces.component.UIComponent)
+     */
+    public void encodeChildren(FacesContext context, UIComponent component)
+            throws IOException {
 
-		if (!component.isRendered()) {
-			return;
-		}
-		RenderUtil.encodeChildren(context, component);
-	}
+        if (!component.isRendered()) {
+            return;
+        }
+        RenderUtil.encodeChildren(context, component);
+    }
 
-	protected void renderAttributes(ResponseWriter writer, UIElement component)
-			throws IOException {
+    protected void renderAttributes(ResponseWriter writer, UIElement component)
+            throws IOException {
 
-		if (component.isIdSet()) {
-			String id = component.getId();
-			RenderUtil.renderAttribute(writer, JsfConstants.ID_ATTR, id, JsfConstants.ID_ATTR);
-		}
-		Map attrs = component.getAttributes();
-		for (Iterator i = attrs.keySet().iterator(); i.hasNext();) {
-			String attrName = (String) i.next();
-			if (attrName.indexOf('.') > 0) {
-				continue;
-			}
-			Object value = component.getAttributes().get(attrName);
-			RenderUtil.renderAttribute(writer, attrName, value, attrName);
-		}
-		String[] bindingPropertyNames = component.getBindingPropertyNames();
-		for (int i = 0; i < bindingPropertyNames.length; ++i) {
-			String name = bindingPropertyNames[i];
-			Object value = BindingUtil.getBindingValue(component, name);
-			RenderUtil.renderAttribute(writer, name, value, name);
-		}
-	}
+        Map attrs = component.getAttributes();
+        for (Iterator i = attrs.keySet().iterator(); i.hasNext();) {
+            String attrName = (String) i.next();
+            if (attrName.indexOf('.') > 0) {
+                continue;
+            }
+            Object value = component.getAttributes().get(attrName);
+            RenderUtil.renderAttribute(writer, attrName, value, attrName);
+        }
+        String[] bindingPropertyNames = component.getBindingPropertyNames();
+        for (int i = 0; i < bindingPropertyNames.length; ++i) {
+            String name = bindingPropertyNames[i];
+            Object value = BindingUtil.getBindingValue(component, name);
+            RenderUtil.renderAttribute(writer, name, value, name);
+        }
+    }
 }
