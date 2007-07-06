377d6b94a2ce2f5292dcef0785b7800b0348b351
Shin Tanimoto <cel@pos.to>
2007-07-06 17:21:28 +0000

    Copyrightの追加など。
    git-svn-id: https://www.seasar.org/svn/s2jsf/branches/s2jsf-1.0.x@867 54d65437-b504-0410-8c2e-c3e44261fb90

--- src/test/java/org/seasar/jsf/mock/MockViewHandler.java
+++ src/test/java/org/seasar/jsf/mock/MockViewHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.jsf.mock;
 
 import java.io.IOException;
@@ -8,21 +23,21 @@ import javax.faces.application.ViewHandler;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
+/**
+ * @author cero-t
+ */
 public class MockViewHandler extends ViewHandler {
     private String actionURL;
 
     public Locale calculateLocale(FacesContext context) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public String calculateRenderKitId(FacesContext context) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public UIViewRoot createView(FacesContext context, String viewId) {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -35,24 +50,17 @@ public class MockViewHandler extends ViewHandler {
     }
 
     public String getResourceURL(FacesContext context, String path) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void renderView(FacesContext context, UIViewRoot viewToRender)
             throws IOException, FacesException {
-        // TODO Auto-generated method stub
-
     }
 
     public UIViewRoot restoreView(FacesContext context, String viewId) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void writeState(FacesContext context) throws IOException {
-        // TODO Auto-generated method stub
-
     }
-
 }
